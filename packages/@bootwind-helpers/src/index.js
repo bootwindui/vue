c64d8889a20f2d62ce4227d04249a8273e5eed96
kewcoder <kewcoder@gmail.com>
2023-10-01 01:44:15 +0700

    add offset height element

--- packages/@bootwind-helpers/src/index.js
+++ packages/@bootwind-helpers/src/index.js
@@ -1,20 +1,27 @@
 
-const getFloating = (btn, element, position, x = 0, y = 0) => {
-    if(btn && element && position){
+const getFloating = (btn, el, position, x = 0, y = 0) => {
+    if(btn && el && position){
 
         const btnWidth = btn.offsetWidth
         const btnTop = btn.offsetTop
         const btnLeft = btn.offsetLeft
 
-       
-        if(position === 'right') {
-            const left = btnWidth + btnLeft + x
-            return `position:fixed;left:${left}px;top:${btnTop + y}px`
+        let top = btnTop + y
+
+        const elTop = el.offsetTop
+        const elHeight = el.offsetHeight
+        if(elTop + elHeight > window.innerHeight){
+            top = window.innerHeight - elHeight - y
         }
 
+        if(position === 'right') {
+            const left = btnWidth + btnLeft + x
+            return `left:${left}px;top:${top}px`
+        }
+        
         if(position === 'left') {
             const right = window.innerWidth - btnLeft + x
-            return `position:fixed;right:${right}px;top:${btnTop + y}px;`
+            return `right:${right}px;top:${top}px`
         }
 
     }
