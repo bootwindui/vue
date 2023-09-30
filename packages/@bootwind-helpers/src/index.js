52f900da338a41bc78af83549cf590449190b04c
kewcoder <kewcoder@gmail.com>
2023-10-01 00:52:53 +0700

    create tooltip helper

--- packages/@bootwind-helpers/src/index.js
+++ packages/@bootwind-helpers/src/index.js
@@ -1,9 +1,27 @@
-const getPosition = (el,el2) =>{
-    return {
-        el: el,
-        el2: el2
+
+const getFloating = (btn, element, position, x = 0, y = 0) => {
+    if(btn && element && position){
+
+        const btnWidth = btn.offsetWidth
+        const btnTop = btn.offsetTop
+        const btnLeft = btn.offsetLeft
+
+       
+        if(position === 'right') {
+            const left = btnWidth + btnLeft + x
+            return `position:fixed;left:${left}px;top:${btnTop + y}px`
+        }
+
+        if(position === 'left') {
+            const right = window.innerWidth - btnLeft + x
+            return `position:fixed;right:${right}px;top:${btnTop + y}px;`
+        }
+
     }
+    return ''
+   
 }
-export default {
-    getPosition
+
+export {
+    getFloating
 }
